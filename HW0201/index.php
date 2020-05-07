c80e3b0bac7c4a5d52b4685d2d350fd74ebd3c72
Anatolii <anatol1812@gmail.com>
2020-05-07 23:19:48 +0300

    Added HW201

---
+++ HW0201/index.php
@@ -0,0 +1,76 @@
+<?php require_once 'utils.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <form action="" method="post">
+        <div class="form-row">
+            <div class="form-group col-md-2 mt-5">
+                <label for="x">x</label>
+                <input type="text" id="x" name="x" require>
+            </div>
+
+            <div class="form-group col-md-1 mt-5">
+
+                <div class="btn-group-vertical" role="group" aria-label="First group">
+                    <label class="btn btn-secondary active">
+                        <input type="radio" name="options" id="option1" autocomplete="off" name="operation" value="plus" checked> +
+                    </label>
+                    <label class="btn btn-secondary">
+                        <input type="radio" name="options" id="option2" autocomplete="off" name="operation" value="minus"> -
+                    </label>
+
+                    <label class="btn btn-secondary">
+                        <input type="radio" name="options" id="option3" autocomplete="off" name="operation" value="multiply" > *
+                    </label>
+
+                    <label class="btn btn-secondary">
+                        <input type="radio" name="options" id="option3" autocomplete="off" name="operation" value="divide" > /
+                    </label>
+
+                    <!-- <button type="button" class="btn btn-secondary">+</button>
+                    <button type="button" class="btn btn-secondary">-</button>
+                    <button type="button" class="btn btn-secondary">*</button>
+                    <button type="button" class="btn btn-secondary">/</button> -->
+                </div>
+            </div>
+
+            <div class="form-group col-md-2 mt-5">
+                <label for="y">y</label>
+                <input type="text" id="y" name="y" require>
+            </div>
+
+            <div class="form-group col-md-1 mt-5">
+                <button type="submit" class="btn btn-primary" id="submitButton">=</button>
+            </div>
+
+            <div class="form-group col-md-2 mt-5">
+                <textarea class="form-control" rows="1" id="comment" readonly><?php 
+                if(isset($error_result)) {
+                    echo "Ошибка: $error_result";
+                 }	
+                else {
+                    echo "Ответ: $result";
+                }
+                ?></textarea>
+            </div>
+
+        </div>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
